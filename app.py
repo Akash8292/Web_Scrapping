d2da46cee6e25f8d58dd422604d625b8d74dd568
Akash_kumar <ak66582919@gmail.com>
2024-08-09 23:23:04 +0530

    first Commit

--- app.py
+++ app.py
@@ -91,4 +91,5 @@ def extract_reviews(url):
         return "No reviews found"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
